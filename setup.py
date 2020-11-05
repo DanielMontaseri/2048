584e6d70a0c8337be873015cf71bc6f0ea9da181
Anthony Wang <ta180m@gmail.com>
2020-11-05 09:51:12 -0600

    More packaging stuff

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="2048-py",
+    version="0.1.4-13",
+    author="Anthony Wang",
+    author_email="ta180m@gmail.com",
+    description="2048 written in Python by the Ladue High School Computer Science Club",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ta180m/2048",
+    py_modules=[ "main" ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        'Topic :: Games/Entertainment :: Puzzle Games',
+    ],
+    entry_points={
+        'console_scripts': [
+            '2048-py = main',
+        ],
+    },
+    install_requires=[ 'termcolor' ]
+)
